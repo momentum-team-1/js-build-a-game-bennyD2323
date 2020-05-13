f68d1dd48af06d9148355e2803f77c586e495b9d
Benjamin D Adams <badams888@hotmail.com>
2020-05-12 20:08:12 -0400

    got my box man movin

--- main.js
+++ main.js
@@ -1,33 +1,70 @@
 
 class Game {
-    constructor() {
+        constructor() {
 
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext('2d');
+        let canvas = document.getElementById("canvas");
+        let ctx = canvas.getContext('2d');
 
-this.bodies = []
-this.bodies.concat(new Player(this, gameSize))
+        let gameSize = {x: canvas.width, y: canvas.height}
+        this.player = new Player(gameSize)
 
-const gameSize = {x: canvas.width, y: canvas.height}
+        let animate = () => {
+            this.update()
+            this.drawPlayer(ctx, gameSize)
+            requestAnimationFrame(animate)
+        }
+        animate()
+    }
 
-addBody(body); {
-    this.bodies.push(body)
-}
+        drawPlayer (ctx, gameSize) {
+            ctx.clearRect(0,0, gameSize.x, gameSize.y)
+            console.log("draw playe rmethod")
+            ctx.fillStyle = "#07beb8"
+            let startingXPosition = this.player.center.x - this.player.size.x / 2
+            let startingYPosition = this.player.center.y - this.player.size.y / 2
+            let playerWidth = this.player.size.x
+            let playerHeight = this.player.size.y
+            ctx.fillRect(startingXPosition, startingYPosition, playerWidth, playerHeight)
+            }
+
+
+    update() {
+        this.player.update()
     }
 }
- 
+
+
 class Player {
-    constructor(game, gameSize) {
-        this.game = game
-        this.size = {x:15, y:15}
-        this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.y * 2 }
+    constructor(gameSize) {
+        
+        this.size = {x:30, y:30}
+        this.center = {x: gameSize.x / 2 , y: gameSize.y - this.size.y * 2 }
+        this.keyboarder = Keyboarder
     }
-}
 
-function drawRectangle (ctx, body) {
-    ctx.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2,
-        body.size.x, body.size.y)
-    ctx.fillStyle = "blue"
+    update () {
+        if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+          console.log("MOVE RIGHT")
+          this.center.x += 4
+        } else if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+          console.log("MOVE LEFT")
+          this.center.x -= 4
+        
+        }
+    
+      }
+    }
+
+
+class Invader {
+    this.size = {x:20, y:30}
+    
 }
+]
+
+
+
+
+
 
 new Game()
