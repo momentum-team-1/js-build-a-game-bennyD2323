5532dd26b0b1ff9ce04ca54cc98b15c41628d16b
Benjamin D Adams <badams888@hotmail.com>
2020-05-13 22:42:34 -0400

    can shoot, working on enemies

--- main.js
+++ main.js
@@ -2,58 +2,48 @@
 class Game {
         constructor() {
 
-        let canvas = document.getElementById("canvas");
+        const canvas = document.getElementById("canvas");
         let ctx = canvas.getContext('2d');
 
         let gameSize = {x: canvas.width, y: canvas.height}
-        this.player = new Player(gameSize)
-        this.bullet = new Bullet(gameSize)
-        let animate = () => {
-            this.update()
-            this.drawPlayer(ctx, gameSize)
-            this.drawBullet(ctx, gameSize)
+        this.player = new Player (this, gameSize)
+        this.bodies = []
+
+        this.bodies = this.bodies.concat(this.player)
+
+        const animate = () => {
+            this.update ()
+            this.drawBodies(ctx, gameSize)
+
             requestAnimationFrame(animate)
-            
         }
+
         animate()
-        }   
+    }   
 
-        drawPlayer (ctx, gameSize) {
-            ctx.clearRect(0,0, gameSize.x, gameSize.y)
-            console.log("draw playe rmethod")
+        drawBodies (ctx, gameSize) {
+        ctx.clearRect (0,0, gameSize.x, gameSize.y)
+        for (let body of this.bodies) {
             ctx.fillStyle = "#07beb8"
-            let startingXPosition = this.player.center.x - this.player.size.x / 2
-            let startingYPosition = this.player.center.y - this.player.size.y / 2
-            let playerWidth = this.player.size.x
-            let playerHeight = this.player.size.y
-            ctx.fillRect(startingXPosition, startingYPosition, playerWidth, playerHeight)
-            }
-        drawBullet (ctx, gameSize) {
-            // ctx.clearRect(0,0, gameSize.x, gameSize.y)
-            
-            ctx.fillStyle = "#000000"
-            let startXBullet = this.player.center.x -this.player.size.x / 2
-            let startYBullet = this.player.center.y - 50
-            let bulletWidth= this.player.size.x
-            let bulletHeight = this.player.size.y
-
-            ctx.fillRect(startXBullet, startYBullet, bulletWidth, bulletHeight)
-            }
-
+            const xPosition = body.center.x - body.size.x / 2
+            const yPosition = body.center.y - body.size.y / 2
+            const width = body.size.x
+            const height = body.size.y
+            ctx.fillRect(xPosition, yPosition, width, height)
+        }
+    }
     update() {
-        this.player.update()
-        
+        for (let body of this.bodies) {
+            body.update()
+        }
     }
 }
-
-
 class Player {
-    constructor(gameSize) {
-        
-        this.size = {x:30, y:30}
-        this.center = {x: gameSize.x / 2 , y: gameSize.y - this.size.y * 2 }
-        this.keyboarder = Keyboarder
-        
+    constructor(game, gameSize) {
+    this.size = {x:30, y:30}
+    this.center = {x: gameSize.x / 2 , y: gameSize.y - this.size.y * 2 }
+    this.keyboarder = Keyboarder
+    this.game = game
     }
 
     update () {
@@ -64,57 +54,46 @@ class Player {
           console.log("MOVE LEFT")
           this.center.x -= 4
         }
-        else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP)) {
-            console.log("MOVE UP")
-            this.center.y -= 4
-        }
-        else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN)) {
-            console.log("MOVE DOWN")
-            this.center.y += 4
-        }
+        // else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP)) {
+        //     console.log("MOVE UP")
+        //     this.center.y -= 4
+        // }
+        // else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN)) {
+        //     console.log("MOVE DOWN")
+        //     this.center.y += 4
+        // }
           if (this.keyboarder.isDown(this.keyboarder.KEYS.S)) {
             console.log("pewpew ")
-            this.bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.y - 10 },
-                { x: 0, y: -7 })
-            }
+            const bullet = new Bullet(
+                { x: this.center.x, y: this.center.y - this.size.y - 10 },
+                { x: 0, y: -7 }
+            )
+            this.game.bodies.push (bullet)
         }
-    }
-    // 
-    
-    
+        }
+}
 
 class Bullet {
     constructor (center, velocity) {
     this.center = center
-    this.size = { x: 10, y: 10}
+    this.size = { x: 7, y: 7}
     this.velocity = velocity
-    this.keyboarder = Keyboarder    
+       
     }
     update() {
         this.center.x += this.velocity.x
         this.center.y += this.velocity.y
-        // if (this.keyboarder.isDown(this.keyboarder.KEYS.S)) {
-        //     console.log("pewpew ")
-        //     this.bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.y - 10 },
-        //         { x: 0, y: -7 })
-            }
-    }
-    
-
-
-
-class Invader {
-    constructor () {
-    this.size = {x:20, y:30}
-    
-    }
+        
+    }   
 }
 
 
-
-// function drawRect (screen, body) {
-//     screen.fillRect(body.center.x - body.size.x / 2, body.center.y - body.size.y / 2,
-//       body.size.x, body.size.y)
+// class Invader {
+//     constructor () {
+//     this.size = {x:20, y:30}
+    
+//     }
+// }
 
 
 new Game()
